827744579271b3133a381510c9c0a11fa253282b
unknown <smritichinta292@gmail.com>
2021-07-11 22:59:37 +0530

    [TIME] timestamp

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class QueryIn(BaseModel):
 # class which is returned in the response
 class QueryOut(BaseModel):
     flower_class: str
-    timestamp: str
+    timestamp: datetime.datetime
 
 # class which is expected in the payload while re-training
 class FeedbackIn(BaseModel):
@@ -46,7 +46,7 @@ def ping():
 # Payload: QueryIn containing the parameters
 # Response: QueryOut containing the flower_class predicted (200)
 def predict_flower(query_data: QueryIn):
-    output = {"flower_class": predict(query_data),timestamp=datetime.now}
+    output = {"flower_class": predict(query_data),timestamp:datetime.datetime.now}
     return output
 
 @app.post("/feedback_loop", status_code=200)
